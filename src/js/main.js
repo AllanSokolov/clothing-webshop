809a178108077c4a2da84ccfb0520c9bce1318b2
Allan Sokolov <sokolov.allan683@gmail.com>
2021-08-31 00:36:16 +0200

    I added scroll to top button and social media section

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,28 @@ window.onclick = function(e) {
   }
 }
 
+// Scroll to top button
+var scrollToTopBtn = document.querySelector(".toTopBtn");
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if ((rootElement.scrollTop / scrollTotal ) > 0.50) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
